8b21fa6edcd1bc047a249fb3dbccf773ed14007a
Mona Lisa <it22284198@my.sliit.lk>
2025-03-02 19:16:51 +0530

    asasa

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ console.log(data);
 }); 
 
 const fs = require('fs'); 
-fs.writeFile('file.txt', 'Hello World!', function (err) { 
+fs.writeFile('file.txt', 'Hello Ravindu!', function (err) { 
   if (err) throw err; 
   console.log('File saved!'); 
 }); 
